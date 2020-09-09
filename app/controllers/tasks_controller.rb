c1003a14f2a64b94576e9fd8c910bf0ce4e59793
s.k <kchan.00400@gmail.com>
2020-09-09 09:40:16 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  before_action :require_user_logged_in
+  before_action :require_user_logged_in 
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.all
@@ -14,7 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
+    @task = current_user.tasks.build(task_params)
 
     if @task.save
       flash[:success] = 'タスクが正常に投稿されました'
@@ -56,5 +57,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content,:status)
   end
-
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end  
